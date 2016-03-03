4fd286632c986b01f7c03f66b45cf82b5d5c8019
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-03-03 09:58:26 +0100

    Solved the stack bug
    Now the child node duty cycle is more apporpriate and predictable,
    without the strange Idle state of the M0. Moreover the preAdvEvent has
    been added, this reduce network latency and power efficency.

--- ST_FIRMWARE/CHILD/Application/CLIMBPerOb.c
+++ ST_FIRMWARE/CHILD/Application/CLIMBPerOb.c
@@ -127,13 +127,15 @@
 #define DEFAULT_CONN_PAUSE_PERIPHERAL         10
 
 // Scan interval value in 0.625ms ticks
-#define SCAN_INTERVAL 						  640//320
+#define SCAN_INTERVAL 						  320
 
 // scan window value in 0.625ms ticks
-#define SCAN_WINDOW							  640//320
+#define SCAN_WINDOW							  320
 
 // Scan duration in ms
-#define DEFAULT_SCAN_DURATION                 2000//10000 //Tempo di durata di una scansione,
+#define DEFAULT_SCAN_DURATION                 1250//10000 //Tempo di durata di una scansione,
+
+#define PRE_ADV_TIMEOUT				  DEFAULT_ADVERTISING_INTERVAL*0.625-5
 
 // Whether to report all contacts or only the first for each device
 #define FILTER_ADV_REPORTS					  FALSE
@@ -201,7 +203,7 @@
 #define EPOCH_EVT							0x0200
 #define WAKEUP_TIMEOUT_EVT					0x0400
 #define GOTOSLEEP_TIMEOUT_EVT				0x0800
-#define RESTART_SCAN_EVT					0x1000
+#define PRE_ADV_EVT					0x1000
 /*********************************************************************
  * TYPEDEFS
  */
@@ -275,7 +277,7 @@ static Clock_Struct connectableTimeoutClock;
 static Clock_Struct wakeUpClock;
 static Clock_Struct goToSleepClock;
 
-static Clock_Struct scanRestartClock;
+static Clock_Struct preAdvClock;
 #ifdef WORKAROUND
 static Clock_Struct epochClock;
 #endif
@@ -382,6 +384,8 @@ static uint8 readyToGoSleeping = 0;
 static uint32 batteryLev = 0;
 
 static uint32 wakeUpTimeout_sec_global;
+
+static uint8 scanning = FALSE;
 /*********************************************************************
  * LOCAL FUNCTIONS
  */
@@ -426,6 +430,7 @@ static void Climb_periodicTask();
 #ifdef WORKAROUND
 static void Climb_epochStartHandler();
 #endif
+static void Climb_preAdvEvtHandler();
 static void Climb_goToSleepHandler();
 static void Climb_wakeUpHandler();
 static void Climb_setWakeUpClock(uint32 wakeUpTimeout_sec_local);
@@ -563,8 +568,8 @@ static void SimpleBLEPeripheral_init(void) {
 	Util_constructClock(&goToSleepClock, Climb_clockHandler,
 	GOTOSLEEP_DEFAULT_TIMEOUT_SEC*1000, 0, false, GOTOSLEEP_TIMEOUT_EVT);
 
-	Util_constructClock(&scanRestartClock, Climb_clockHandler,
-	DEFAULT_SCAN_DURATION, 0, false, RESTART_SCAN_EVT);
+	Util_constructClock(&preAdvClock, Climb_clockHandler,
+	PRE_ADV_TIMEOUT, 0, false, PRE_ADV_EVT);
 
 #ifdef WORKAROUND
 	Util_constructClock(&epochClock, Climb_clockHandler,
@@ -821,10 +826,11 @@ static void SimpleBLEPeripheral_taskFxn(UArg a0, UArg a1)
 		Climb_wakeUpHandler();
 	}
 
-	if (events & RESTART_SCAN_EVT) {
-		events &= ~RESTART_SCAN_EVT;
+	if (events & PRE_ADV_EVT) {
+		events &= ~PRE_ADV_EVT;
 
-		GAPRole_StartDiscovery(DEFAULT_DISCOVERY_MODE, DEFAULT_DISCOVERY_ACTIVE_SCAN, DEFAULT_DISCOVERY_WHITE_LIST);
+		Climb_preAdvEvtHandler();
+
 
 	}
 
@@ -1233,17 +1239,7 @@ static void BLE_ConnectionEventHandler(void)
  */
 static void BLE_AdvertiseEventHandler(void) {
 
-#ifdef CLIMB_DEBUG
-	adv_counter++; //non era incrementato....
-
-	if ((adv_counter - 1) % 60 == 0) {
-		batteryLev = AONBatMonBatteryVoltageGet();
-		batteryLev = (batteryLev * 125) >> 5;
-	}
-
-	Climb_updateMyBroadcastedState(nodeState); //update adv data every adv event to update adv_counter value. Since the argument is nodeState this function call doesn't modify the actual state of this node
-#endif
-
+	Util_startClock(&preAdvClock);
 
 #ifdef WORKAROUND
 	uint8 adv_active = 0;
@@ -1392,11 +1388,15 @@ static void SimpleBLEObserver_processRoleEvent(gapObserverRoleEvent_t *pEvent) {
 
 	case GAP_DEVICE_DISCOVERY_EVENT:
 #ifndef WORKAROUND
-		if(beaconActive){
+
+		scanning = FALSE;
+
+
+
 			//GAPRole_StartDiscovery(DEFAULT_DISCOVERY_MODE,DEFAULT_DISCOVERY_ACTIVE_SCAN, DEFAULT_DISCOVERY_WHITE_LIST);
-			Util_startClock(&scanRestartClock);
+			//Util_startClock(&preAdvClock);
 			CLIMB_FlashLed(Board_LED2);
-		}
+
 #endif
 		break;
 	case GAP_ADV_DATA_UPDATE_DONE_EVENT:
@@ -2137,6 +2137,27 @@ static void Climb_epochStartHandler(){
 	}
 }
 #endif
+
+static void Climb_preAdvEvtHandler(){
+
+#ifdef CLIMB_DEBUG
+	adv_counter++;
+
+	if ((adv_counter - 1) % 60 == 0) {
+		batteryLev = AONBatMonBatteryVoltageGet();
+		batteryLev = (batteryLev * 125) >> 5;
+	}
+
+	Climb_updateMyBroadcastedState(nodeState); //update adv data every adv event to update adv_counter value. Since the argument is nodeState this function call doesn't modify the actual state of this node
+#endif
+
+	if(!scanning){
+		if(beaconActive){
+			GAPRole_StartDiscovery(DEFAULT_DISCOVERY_MODE, DEFAULT_DISCOVERY_ACTIVE_SCAN, DEFAULT_DISCOVERY_WHITE_LIST);
+			scanning = TRUE;
+		}
+	}
+}
 /*********************************************************************
  * @fn      Climb_goToSleepHandler
  *
@@ -2329,6 +2350,7 @@ static void startNode() {
 static void stopNode() {
 	beaconActive = 0;
 	GAPObserverRole_CancelDiscovery();
+	scanning = FALSE;
 	uint8 adv_active = 0;
 	uint8 status = GAPRole_SetParameter(GAPROLE_ADV_NONCONN_ENABLED, sizeof(uint8_t), &adv_active);
 
