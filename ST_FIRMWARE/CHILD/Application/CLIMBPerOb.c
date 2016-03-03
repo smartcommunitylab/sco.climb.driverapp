0718b4287323beeb056319eab60e89921b132cf1
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-03-03 13:10:24 +0100

    Added GOING_TO_SLEEP state on child nodes
    By setting the nodes to GOING_TO_SLEEP state they start a timer and they
    go to sleep.

--- ST_FIRMWARE/CHILD/Application/CLIMBPerOb.c
+++ ST_FIRMWARE/CHILD/Application/CLIMBPerOb.c
@@ -162,6 +162,7 @@
 #define CONNECTABLE_TIMEOUT_MSEC					1000*60
 #define WAKEUP_DEFAULT_TIMEOUT_SEC				60*60*24//1000*60*60
 #define GOTOSLEEP_DEFAULT_TIMEOUT_SEC			60*60//1000*60*60
+#define GOING_TO_SLEEP_DELAY_AFTER_REQUEST_MSEC 1000*30
 
 #define NODE_ID								  { 0x02  }
 
@@ -213,6 +214,7 @@ typedef enum ChildClimbNodeStateType_t {
 	CHECKING,
 	ON_BOARD,
 	ALERT,
+	GOING_TO_SLEEP,
 	ERROR,
 	INVALID_STATE
 } ChildClimbNodeStateType_t;
@@ -379,7 +381,7 @@ static uint8 adv_counter = 0;
 static uint8 myIDArray[] = {0x00};//NODE_ID;
 static uint8 broadcastID[] = { 0xFF };
 
-static uint8 readyToGoSleeping = 0;
+//static uint8 readyToGoSleeping = 0;
 
 static uint32 batteryLev = 0;
 
@@ -1642,74 +1644,82 @@ static uint8 Climb_checkNodeState(gapDeviceInfoEvent_t *gapDeviceInfoEvent_a) {
 	ChildClimbNodeStateType_t broadcastedState;
 
 	//OBTAIN broadcastedState and check where the message is from (myMaster or not)
-	if( nodeState == CHECKING || nodeState == ON_BOARD || nodeState == ALERT ){
-		if (memcomp(gapDeviceInfoEvent_a->addr, myMasterAddr,	B_ADDR_LEN) == 0){ //sto analizzando un adv del MIO master
+	if (nodeState == CHECKING || nodeState == ON_BOARD || nodeState == ALERT) {
+		if (memcomp(gapDeviceInfoEvent_a->addr, myMasterAddr, B_ADDR_LEN) == 0) { //sto analizzando un adv del MIO master
 			broadcastedState = Climb_findMyBroadcastedState(gapDeviceInfoEvent_a);
-		}else{	//sto analizzando l'adv di un altro master,
+		} else {	//sto analizzando l'adv di un altro master,
 			return 0;
 		}
-	}else{ //se sono in by myself cerca in tutti i master visibili (il primo master che mi farà fare il checking diventerà il MIO master)
+	} else { //se sono in by myself cerca in tutti i master visibili (il primo master che mi farà fare il checking diventerà il MIO master)
 		broadcastedState = Climb_findMyBroadcastedState(gapDeviceInfoEvent_a);
 	}
 
 	//VERIFY STATE TRANSITIONS
 	//if( broadcastedState != nodeState || broadcastedState == INVALID_STATE){
-		uint8 i;
-		switch (broadcastedState) {
-		case BY_MYSELF: //CHECK OUT
-			//if (nodeState == ON_BOARD || nodeState == CHECKING) { //il checkout è possibile da qualsiasi stato
-			for (i = 0; i < B_ADDR_LEN; i++) { //resetta l'indirizzo del nodo master
-				myMasterAddr[i] = 0;
-			}
-
-			if (nodeState == ON_BOARD || nodeState == ALERT) { //il passaggio da ON_BOARD o (ALERT) a BY_MYSELF triggera lo spegnimento del nodo
-				Util_restartClock(&goToSleepClock, 20000); //schedula lo spegnimento automatico
-				readyToGoSleeping = 1;
-			}
-			//}
-			break;
-		case CHECKING:
-			if (nodeState == BY_MYSELF) {
-				if (readyToGoSleeping == 0) {
-					memcpy(myMasterAddr, gapDeviceInfoEvent_a->addr, B_ADDR_LEN); //salva l'indirizzo del nodo master
-				} else { //se il master sta cercando di fare un check in subito dopo il check out non permetterlo!
-					//broadcastedState = nodeState;
-					return 0;
-				}
-			}else{ //se il master sta cercando di mettere il nodo in checking ma questo non era in by myself scarta la richiesta
-				//broadcastedState = nodeState;
-				return 0;
-			}
-			break;
-
-		case ON_BOARD:
-			if (nodeState == CHECKING || nodeState == ALERT) { //il passaggio in ON BOARD è permesso solo dagli stati checking e alert
-
-			}else{ //se il master sta cercando di mettere il nodo in checking ma questo non era in by myself scarta la richiesta
-				//broadcastedState = nodeState;
-				return 0;
-			}
-			break;
-
-		case ALERT:
-			return 0;
-			//break;
-
-		case INVALID_STATE://check to see if there is a broadcast message in this packet
-			if (nodeState == BY_MYSELF) { //broadcast message can be received only if I have a myMaster Node
-				//broadcastedState = nodeState;
-				return 0;
-			} else {
-				Climb_decodeBroadcastedMessage(gapDeviceInfoEvent_a);
-				return 0;
-			}
-			//break;
-
-		default:
-			return 0;
-			//break;
+	uint8 i;
+	switch (broadcastedState) {
+	case BY_MYSELF: //CHECK OUT
+		//if (nodeState == ON_BOARD || nodeState == CHECKING) { //il checkout è possibile da qualsiasi stato
+		for (i = 0; i < B_ADDR_LEN; i++) { //resetta l'indirizzo del nodo master
+			myMasterAddr[i] = 0;
 		}
+		if(nodeState == GOING_TO_SLEEP){
+			Util_restartClock(&goToSleepClock, GOTOSLEEP_DEFAULT_TIMEOUT_SEC*1000);
+		}
+		//if (nodeState == ON_BOARD || nodeState == ALERT) { //il passaggio da ON_BOARD o (ALERT) a BY_MYSELF triggera lo spegnimento del nodo
+			//Util_restartClock(&goToSleepClock, 20000); //schedula lo spegnimento automatico
+			//readyToGoSleeping = 1;
+		//}
+		//}
+		break;
+	case CHECKING:
+		if (nodeState == BY_MYSELF) {
+			//if (readyToGoSleeping == 0) {
+			memcpy(myMasterAddr, gapDeviceInfoEvent_a->addr, B_ADDR_LEN); //salva l'indirizzo del nodo master
 
+		} else { //se il master sta cercando di mettere il nodo in checking ma questo non era in by myself scarta la richiesta
+				 //broadcastedState = nodeState;
+			return 0;
+		}
+		break;
+
+	case ON_BOARD:
+		if (nodeState == CHECKING || nodeState == ALERT) { //il passaggio in ON BOARD è permesso solo dagli stati checking e alert
+
+		} else { //se il master sta cercando di mettere il nodo in checking ma questo non era in by myself scarta la richiesta
+				 //broadcastedState = nodeState;
+			return 0;
+		}
+		break;
+
+	case ALERT:
+		return 0;
+		//break;
+
+	case GOING_TO_SLEEP:
+		if (nodeState == ON_BOARD || nodeState == CHECKING) { //il passaggio da ON_BOARD o (ALERT) a GOING_TO_SLEEP triggera lo spegnimento del nodo
+			Util_restartClock(&goToSleepClock, GOING_TO_SLEEP_DELAY_AFTER_REQUEST_MSEC); //schedula lo spegnimento automatico
+			//readyToGoSleeping = 1;
+		} else {
+			//broadcastedState = nodeState;
+			return 0;
+		}
+		break;
+
+	case INVALID_STATE:				//check to see if there is a broadcast message in this packet
+		if (nodeState == BY_MYSELF) { //broadcast message can be received only if I have a myMaster Node
+			//broadcastedState = nodeState;
+			return 0;
+		} else {
+			Climb_decodeBroadcastedMessage(gapDeviceInfoEvent_a);
+			return 0;
+		}
+		//break;
+
+	default:
+		return 0;
+		//break;
+	}
 
 //		if(nodeState == BY_MYSELF && broadcastedState == CHECKING){ //ho trovato il master (pedibus driver) di oggi. se readyToGoSleeping è diverso da 0 significa che ho gia fatto un checkout, quindi non devo tornare ON_BOARD
 //			if(readyToGoSleeping == 0){
@@ -1730,12 +1740,12 @@ static uint8 Climb_checkNodeState(gapDeviceInfoEvent_t *gapDeviceInfoEvent_a) {
 //		}
 
 #ifndef CLIMB_DEBUG
-		Climb_updateMyBroadcastedState(broadcastedState);
+	Climb_updateMyBroadcastedState(broadcastedState);
 #else	//quando il sistema è in modalità debug si aggiorna solo la variabile nodeState, la funzione Climb_updateMyBroadcastedState che aggiorna anche l'adv verrà chiamata dalla adv_event_handler
-		nodeState = broadcastedState;
+	nodeState = broadcastedState;
 #endif
 
-		return 1;
+	return 1;
 	//}
 	//return 0;
 }
@@ -2323,7 +2333,7 @@ static void CLIMB_handleKeys(uint8 keys) {
 static void startNode() {
 
 	if (beaconActive != 1) {
-		readyToGoSleeping = 0;
+		//readyToGoSleeping = 0;
 
 		uint8 adv_active = 1;
 		uint8 status = GAPRole_SetParameter(GAPROLE_ADV_NONCONN_ENABLED, sizeof(uint8_t), &adv_active);
