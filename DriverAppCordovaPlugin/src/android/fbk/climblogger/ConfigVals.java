c25bbf1791be34ca7da2e1146058611ce36047fe
Davide Giovanelli <dgiovanelli@fbk.eu>
2017-02-09 11:53:39 +0100

    lib version 0.2.3

--- DriverAppCordovaPlugin/src/android/fbk/climblogger/ConfigVals.java
+++ DriverAppCordovaPlugin/src/android/fbk/climblogger/ConfigVals.java
@@ -12,7 +12,7 @@ import java.util.UUID;
  */
 public class ConfigVals {
 
-    static String libVersion = "0.2.2";
+    static String libVersion = "0.2.3";
 
     private static File root = Environment.getExternalStorageDirectory();
 
