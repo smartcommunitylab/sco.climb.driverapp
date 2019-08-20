90a543aa7043439a1d78335019491501c1d631c9
kazhamiakin <kazhamiakin@gmail.com>
2019-08-20 11:51:00 +0200

    Minor fixes

--- DriverApp/www/js/climb-dap-run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#cp -rv ~/workspaces/climb/DriverAppCordovaPlugin/src/* ~/Projects/CLIMB/sco.climb.driverapp/DriverAppCordovaPlugin/src/android/
-cd ~/Projects/CLIMB/sco.climb.driverapp/DriverApp
-cordova plugin remove it.smartcommunitylab.climb.dap && cordova plugin add ../DriverAppCordovaPlugin && ionic run android
